451ce192dd66b664d7bace55d1e52aa6faa10ec3
RichardMasson <richardmassn@gmail.com>
2021-09-20 00:06:34 +0200

    Prediction class first prototype.

--- AD_Predict.py
+++ AD_Predict.py
@@ -3,24 +3,27 @@ import tensorflow as tf
 import numpy as np
 from scipy import ndimage
 from tensorflow import keras
-from AD_Train import validation_preprocessing
 from sklearn.utils import shuffle
 
 # Fetch the saved testing data
 test = np.load('testing.npz')
 x_test = test['a']
 y_test = test['b']
+print("Data loaded.")
 
 # Shuffle the test sets (for more varied testing)
 x_test_shuff, y_test_shuff = shuffle(np.array(x_test), np.array(y_test))
 
 # Quick test
-print(y_test)
-'''
+print(y_test_shuff)
+
 # Load up the model
-model = model.load_weights('ADModel')
-for i in range(0,4):
+model = keras.models.load_model('ADModel')
+classes = ['normal', 'mild impairment', 'dementia']
+for i in range(0, min(4, len(y_test_shuff))):
     prediction = model.predict(np.expand_dims(x_test_shuff[i], axis=0))[0]
-    print("The model predicts that this scan is type:", prediction[0], "| Actual cdr value:", y_test_shuff[i])
+    print("The model predicts the following:")
+    for j in range(0, len(prediction)):
+        print(round(prediction[j],2), "\% ", classes[j], ", ", sep='', end='')
+    print("\nActual:", y_test_shuff[i])
 print("Done.")
-'''
