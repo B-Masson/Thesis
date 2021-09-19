b79305969d1543168fb2b27f4b0f07cbce496794
RichardMasson <richardmassn@gmail.com>
2021-09-19 21:17:50 +0200

    Prediction class refinement.

--- AD_Predict.py
+++ AD_Predict.py
@@ -4,7 +4,23 @@ import numpy as np
 from scipy import ndimage
 from tensorflow import keras
 from AD_Train import validation_preprocessing
+from sklearn.utils import shuffle
 
+# Fetch the saved testing data
 test = np.load('testing.npz')
 x_test = test['a']
 y_test = test['b']
+
+# Shuffle the test sets (for more varied testing)
+x_test_shuff, y_test_shuff = shuffle(np.array(x_test), np.array(y_test))
+
+# Quick test
+print(y_test)
+'''
+# Load up the model
+model = model.load_weights('ADModel')
+for i in range(0,4):
+    prediction = model.predict(np.expand_dims(x_test_shuff[i], axis=0))[0]
+    print("The model predicts that this scan is type:", prediction[0], "| Actual cdr value:", y_test_shuff[i])
+print("Done.")
+'''
