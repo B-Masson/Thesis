a8204b11b05d6a0a7e78bf87299afbe6f624c682
RichardMasson <richardmassn@gmail.com>
2022-08-03 13:56:30 +0200

    Pre-processing stuff.

---
+++ CheckStrips.py
@@ -0,0 +1,41 @@
+# Check what's up with the skull stripping process
+import os
+import os.path as op
+import re
+
+root = "/scratch/mssric004/ADNI_Data_NIFTI"
+#root = "/scratch/mssric004/ADNI_Data_NIFTI"
+stripped_images = 0
+total_images = 0
+red_flag = 0
+double1 = 0
+double2 = 0
+
+
+class_dirs = os.listdir(root)
+for classes in class_dirs:
+	if classes != "Zips":
+		for scan in os.listdir(op.join(root, classes)):
+			for type in os.listdir(op.join(root, classes, scan)):
+				for date in os.listdir(op.join(root, classes, scan, type)):
+					for image_folder in os.listdir(op.join(root, classes, scan, type, date)):
+						image_root = op.join(root, classes, scan, type, date, image_folder)
+						image_files = os.listdir(image_root)
+						if len(image_files) != 0:
+							total_images += 1
+						red_flag = 0
+						for file in image_files:
+							if "STRIPPED" in file:
+								stripped_images += 1
+								red_flag += 1
+								if (red_flag > 1):
+									double1 += 1
+							if "STRIPPED_STRIPPED" in file:
+								double2 += 1
+						if (red_flag == 0):
+							print("No strips found in", image_root, "\n", image_files)
+									
+print("There are a total of", stripped_images, "stripped out of", total_images)
+print("Can confirm there are", double1, "doubled entries.", double2, "of which are stripped_stripped.")
+print("Factoring this in puts as at:", (stripped_images-double1), "/", total_images, "correct strips.")
+print("All done.")
---
+++ DimensionChecker.py
@@ -0,0 +1,44 @@
+import nibabel as nib
+import numpy as np
+from scipy import stats
+import os.path as op
+
+dirs = []
+#dirs.append("Directories/test_adni_1_images.txt")
+dirs.append("Directories/adni_1_images.txt")
+dirs.append("Directories/adni_1_images_normed.txt")
+dirs.append("Directories/adni_3_images.txt")
+dirs.append("Directories/adni_3_images_normed.txt")
+
+for dir_file in dirs:
+    # Grab the data
+    print("READING FROM:", dir_file)
+    if op.exists(dir_file):
+        path_file = open(dir_file, "r")
+        path = path_file.read()
+        path = path.split("\n")
+        path_file.close()
+
+        # Dimension counts
+        hcount = []
+        wcount = []
+        dcount = []
+        counter = 0
+
+        for file in path:
+            nifti = np.asarray(nib.load(file).get_fdata())
+            #print("Shape:", nifti.shape)
+            hcount.append(nifti.shape[0])
+            wcount.append(nifti.shape[1])
+            dcount.append(nifti.shape[2])
+            counter += 1
+
+        print("Analysed a total of", counter, "images.")
+        have = (int)(np.sum(hcount)/counter)
+        wave = (int)(np.sum(wcount)/counter)
+        dave = (int)(np.sum(dcount)/counter)
+        print("Average dimensions: ( ", have, ", ", wave, ", ", dave, " )", sep='')
+        hmode = stats.mode(hcount)
+        wmode = stats.mode(wcount)
+        dmode = stats.mode(dcount)
+        print("Modal dimensions:\n", hmode, "\n", wmode, "\n", dmode, "\n")
